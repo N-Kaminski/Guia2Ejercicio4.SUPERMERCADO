9b7fb68c767261bf279d534bf3be2284cc6d4426
Nico <murek38@gmail.com>
2023-08-14 19:14:51 -0300

    Initial commit

---
+++ src/guia2video/pkgsuper/Guia2VideoSuper.java
@@ -0,0 +1,15 @@
+
+package guia2video.pkgsuper;
+
+// @author Nicolas Kaminski 
+
+
+public class Guia2VideoSuper {
+
+
+    public static void main(String[] args) {
+        
+
+    }       // lave main
+
+}       // llave clase
---
+++ src/guia2video/pkgsuper/Producto.java
@@ -0,0 +1,73 @@
+package guia2video.pkgsuper;
+
+/**
+ * @author Nicolas Kaminski
+ */
+public class Producto implements Comparable<Producto> {
+
+    private int codigo, stock;
+    private String descripcion;
+    private double precio;
+    private Categoria rubro;
+
+    public Producto(int codigo, int stock, String descripcion, double precio, Categoria rubro) {
+        this.codigo = codigo;
+        this.stock = stock;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.rubro = rubro;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Categoria getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(Categoria rubro) {
+        this.rubro = rubro;
+    }
+
+    @Override
+    public int compareTo(Producto x) {
+        if (codigo == x.codigo) {
+            return 0;
+        } else if (codigo > x.codigo) {
+            return 1;
+        } else {
+            return -1;
+        }
+
+    }
+
+}  // LLAVE DE CLASE
